84284cd2409596ec4b48af65e8ab52a056e756b2
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-29 23:16:09 -0600

    fix: add opencv shi

--- flake.nix
+++ flake.nix
@@ -1,109 +1,58 @@
 {
-  description = "Development environment for ASTRA Autonomy";
-
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
+    astra-msgs.url = "github:SHC-ASTRA/astra_msgs/main";
 
-    treefmt-nix = {
+    treefmt = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    unilib = {
-      url = "github:SHC-ASTRA/unilib/1c4834be48c4d650fe43e88255c59fc087eaaac1";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "nix-ros-overlay/flake-utils";
-      inputs.treefmt.follows = "treefmt-nix";
-    };
   };
-
-  outputs =
-    {
-      self,
-      nix-ros-overlay,
-      nixpkgs,
-      unilib,
-      ...
-    }@inputs:
-    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nix-ros-overlay, nixpkgs, astra-msgs, ... }@inputs:
+    nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (system:
       let
+        applyDistroOverlay = rosOverlay: rosPackages:
+          rosPackages // builtins.mapAttrs (rosDistro: rosPkgs:
+            if rosPkgs ? overrideScope then
+              rosPkgs.overrideScope rosOverlay
+            else
+              rosPkgs) rosPackages;
+        rosDistroOverlays = final: prev: {
+          # Overlay opencv4 to build it with GTK2 for GUI
+          opencv4 = prev.opencv4.override { enableGtk2 = true; };
+
+          # Apply the overlay to multiple ROS distributions
+          rosPackages =
+            applyDistroOverlay (import ./overlay.nix) prev.rosPackages;
+        };
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ nix-ros-overlay.overlays.default ];
+          overlays = [ nix-ros-overlay.overlays.default rosDistroOverlays ];
         };
-        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "ASTRA Anchor";
-          packages = with pkgs; [
-            colcon
-            socat
-            can-utils
-            (python313.withPackages (
-              p: with p; [
-                pyserial
-                python-can
-                pygame
-                scipy
-                crccheck
-                black
-                unilib.packages.${system}.unilib
-              ]
-            ))
-            (
-              with rosPackages.humble;
-              buildEnv {
-                paths = [
-                  ros-core
-                  ros2cli
-                  ros2run
-                  ros2bag
-                  rviz2
-                  xacro
-                  ament-cmake-core
-                  python-cmake-module
-                  diff-drive-controller
-                  parameter-traits
-                  generate-parameter-library
-                  joint-state-publisher-gui
-                  robot-state-publisher
-                  ros2-control
-                  controller-manager
-                  control-msgs
-                  control-toolbox
-                  topic-based-ros2-control
-                  pilz-industrial-motion-planner
-                  pick-ik
-                  ompl
-                  joy
-                  ros2-controllers
-                  chomp-motion-planner
-                  cv-bridge
-                ];
-              }
-            )
-          ];
-          shellHook = ''
-            # Display stuff
-            export DISPLAY=''${DISPLAY:-:0}
-            export QT_X11_NO_MITSHM=1
-          '';
+        astra_msgs_pkgs = astra-msgs.packages.${system};
+
+        rosDistro = "humble";
+
+      in {
+        legacyPackages = pkgs.rosPackages;
+        packages = builtins.intersectAttrs (import ./overlay.nix null null)
+          pkgs.rosPackages.${rosDistro};
+        checks = builtins.intersectAttrs (import ./overlay.nix null null)
+          pkgs.rosPackages.${rosDistro};
+        devShells.default = import ./shell.nix {
+          inherit pkgs rosDistro;
+          extraPkgs = { };
+          extraPaths = [ astra_msgs_pkgs.astra-msgs ];
         };
 
-        checks.formatting = treefmtEval.config.build.check self;
-        formatter = treefmtEval.config.build.wrapper;
-      }
-    );
+        formatter = (inputs.treefmt.lib.evalModule pkgs
+          ./treefmt.nix).config.build.wrapper;
 
+      });
   nixConfig = {
-    # Cache to pull ros packages from
-    extra-substituters = [
-      "https://ros.cachix.org"
-      "https://attic.iid.ciirc.cvut.cz/ros"
-    ];
+    extra-substituters =
+      [ "https://ros.cachix.org" "https://attic.iid.ciirc.cvut.cz/ros" ];
     extra-trusted-public-keys = [
       "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
       "ros:JR95vUYsShSqfA1VTYoFt1Nz6uXasm5QrcOsGry9f6Q="
---
+++ overlay.nix
@@ -0,0 +1,7 @@
+final: prev: {
+  clucky-detection = final.callPackage ././clucky_detection/package.nix { };
+  clucky-localization =
+    final.callPackage ././clucky_localization/package.nix { };
+  clucky-nav2 =
+    final.callPackage ././install/clucky_nav2/share/clucky_nav2/package.nix { };
+}
